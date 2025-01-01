90ba5e3f184fd9ad1cc5b741144996e5c39b71a2
sumitsongh160@gmail.com <sumitsongh160@gmail.con>
2025-01-01 13:37:00 +0530

    Added a new file Storage

---
+++ Storage.js
@@ -0,0 +1,10 @@
+let todo = [
+  {
+    id: 1,
+    task: "Go to gym",
+    description: "Go to gym before 2 pm",
+    complete: false,
+  },
+];
+
+module.exports = todo;
--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 const express = require("express");
 const app = express();
 
+// Import the todo array from storage.js
+const todo = require("./Storage");
 
-let todo = [{
-  task:"Go to Gym",
-  description:"Got to gym at 2 pm daily",
-  complete: false,
-  id:1,
-}]
+app.get('/todo', (req, res) => {     
+    // jab bho /todo route par get request aayegi, initially jab yeh search hoga too by default hi get request send ho jayegi 
+    res.send(todo);
+});
 
-
-  app.get('/todo',(req,res)=> {     // jab bho /todo route par get request aayegi, initially jab yeh search hoga too by default hi get request send ho jayegi 
-      res.send(todo);
-  })
-
-  app.post('/todo',(req,res)=> { 
-// jab /todo par post request aayegi ko jo bhi existing todo hoga usme new todo add kr denge,
-// humne example ke liye hardcoded todo liya hai but in actuall scenerio hum new todo ko input lete hain and use as an request parameter backend ko send krte hain
+app.post('/todo', (req, res) => { 
+    // jab /todo par post request aayegi ko jo bhi existing todo hoga usme new todo add kr denge,
+    // humne example ke liye hardcoded todo liya hai but in actuall scenerio hum new todo ko input lete hain and use as an request parameter backend ko send krte hain
     const newtodo = {
-      task:"Complete Homework till 3pm",
-      description:"Both dsa and web-dev assignments ",
-      complete: false,
-      id: 2,
+        task: "Complete Homework till 3pm",
+        description: "Both dsa and web-dev assignments ",
+        complete: false,
+        id: 2,
     };
 
     todo.push(newtodo); 
     res.send(todo);
-  })
+});
 
-  app.put("/todo", (req, res) => {
+app.put("/todo", (req, res) => {
     // same post request method ki trah in case pur request aati hai to hum use particular id wale todo ko update kr denge 
 
     const id = 2; 
@@ -37,12 +32,11 @@ let todo = [{
     const todoItem = todo.find((item) => item.id === id);   // find function will run through the complete todo and search for the first todoitem with same id mentioned
   
     if (todoItem) {
-      todoItem.description = newDescription;
-      res.send(todo); // Return the updated todo
+        todoItem.description = newDescription;
+        res.send(todo); // Return the updated todo
     } else {
-      res.status(404).send({ error: "Todo not found" }); // in case todo is not found we will send a json to the frontend and a status code of 404 means there is something wrong from the side of client
+        res.status(404).send({ error: "Todo not found" }); // in case todo is not found we will send a json to the frontend and a status code of 404 means there is something wrong from the side of client
     }
-  });
-  
+});
 
 app.listen(3000);
